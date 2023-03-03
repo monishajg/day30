a9190d42d6f5671d9fbba12140a27e77b9fd3c46
monishajg <monishajg@gmail.com>
2023-03-03 16:31:57 +0800

    task4

--- src/main/java/ibf2022/paf/day30/controller/FundsTransferController.java
+++ src/main/java/ibf2022/paf/day30/controller/FundsTransferController.java
@@ -36,6 +36,36 @@ public class FundsTransferController {
     public String processTransfer (@Valid @ModelAttribute("transfer") Transfer transfer, BindingResult bind, Model model) {
     if(bind.hasErrors()){
     model.addAttribute("transfer", transfer);
+    
+    public String transferFunds(@RequestParam("from") String fromAccount,
+    @RequestParam("to") String toAccount,
+    @RequestParam("amount") double amount) {
+
+    // check if both accounts exist
+    if (!checkAccountExistence(fromAccount) || !checkAccountExistence(toAccount)) {
+    return "One or both of the accounts do not exist.";
+    }
+    
+    // check account number length
+    if (!checkAccountNumberLength(fromAccount) || !checkAccountNumberLength(toAccount)) {
+    return "One or both of the account numbers are not 10 characters long.";
+    }
+    
+    // perform transfer
+    // transferFunds(fromAccount, toAccount, amount);
+    
+    return "Funds transfer successful.";
+    }
+    
+    private boolean checkAccountExistence(String accountNumber) {
+    // check if account exists
+    return true; // replace with actual implementation
+    }
+    
+    private boolean checkAccountNumberLength(String accountNumber) {
+    return accountNumber.length() == 10;
+    }
+    
     return "index";
     }
     return "view1";
